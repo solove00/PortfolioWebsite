28a988be284f5a6c2df5f1247fe932dca5868a53
Shan Ou <oushanmail@gmail.com>
2018-04-06 14:19:04 -0400

    Make nav bar responsive

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function responsiveNavBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
