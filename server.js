0c6c007ccc72c35fbe08a25a6e4211bdbe223368
Shan Ou <oushanmail@gmail.com>
2018-04-27 15:19:30 -0400

    fix exipration

--- server.js
+++ server.js
@@ -40,8 +40,39 @@
 
 // console.log('HTTP server listening on port 80');
 
-var app = express.createServer();
-self.app.all(/.*/, function(req, res, next) {
+// const express = require('express');
+
+// var app = express();
+
+// pp.set('port', (process.env.PORT || 5000))
+// app.use(express.static(__dirname + '/public'))
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// })
+
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+// app.all(/.*/, function(req, res, next) {
+//   var host = req.header("host");
+//   if (host.match(/^www\..*/i)) {
+//     next();
+//   } else {
+//     res.redirect(301, "http://www." + host + req.url);
+//   }
+// });
+
+
+
+// app.use('/',express.static('public', { maxage: '1d' }));
+
+var express = require("express");
+var app = express();
+
+//Redirect web.com to www.web.com
+app.all(/.*/, function (req, res, next) {
   var host = req.header("host");
   if (host.match(/^www\..*/i)) {
     next();
@@ -49,4 +80,33 @@ self.app.all(/.*/, function(req, res, next) {
     res.redirect(301, "http://www." + host + req.url);
   }
 });
-app.use('/',express.static('public', { maxage: '1d' }));
+
+/* serves main page */
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function (req, res) {
+  console.log('static file request : ' + req.params);
+  if (req.url.indexOf("/images/") === 0 || req.url.indexOf("/css/") === 0 || req.url.indexOf("/js/") === 0) {
+    res.setHeader("Cache-Control", "public, max-age=3600000");
+    res.setHeader("Expires", new Date(Date.now() + 3600000).toUTCString());
+  }
+  res.sendFile(__dirname + req.params[0]);
+});
+
+// app.get('/*', function (req, res, next) {
+
+//   if (req.url.indexOf("/images/") === 0 || req.url.indexOf("/css/") === 0) {
+//     res.setHeader("Cache-Control", "public, max-age=3600000");
+//     res.setHeader("Expires", new Date(Date.now() + 3600000).toUTCString());
+//   }
+//   next();
+// });
+
+var port = process.env.PORT || 3333;
+
+app.listen(port, function () {
+  console.log("Listening on " + port);
+});
