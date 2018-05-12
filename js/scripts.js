9e8d34efb32398aeeea2819a523bc56d23ca1b59
Shan <anna.shan.ou@gmail.com>
2018-05-12 14:06:27 -0400

    update case study

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ function responsiveNavBar() {
         x.className = "topnav";
     }
 }
-
-window.onscroll = function (ev) {
-    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 1000) {
-        var x = document.getElementById("prototype_link");
-        if (x != undefined) {
-            this.console.log(x);
-            x.classList.add("textglow");
-        }
-    }
-    else {
-        var x = document.getElementById("prototype_link");
-        if (x != undefined) {
-            this.console.log(x);
-            x.classList.remove("textglow");
-        }
-    }
-};
+// 
+// window.onscroll = function (ev) {
+//     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 1000) {
+//         var x = document.getElementById("prototype_link");
+//         if (x != undefined) {
+//             this.console.log(x);
+//             x.classList.add("textglow");
+//         }
+//     }
+//     else {
+//         var x = document.getElementById("prototype_link");
+//         if (x != undefined) {
+//             this.console.log(x);
+//             x.classList.remove("textglow");
+//         }
+//     }
+// };
